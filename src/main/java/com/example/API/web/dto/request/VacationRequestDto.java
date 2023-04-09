b3ec5ad64bf15ef5fb5fdba7c28d44b9eb0d7818
drgj <dydwns7936@naver.com>
2023-04-10 00:38:33 +0900

    vacation 코드 업그레이드

--- src/main/java/com/example/API/web/dto/request/VacationRequestDto.java
+++ src/main/java/com/example/API/web/dto/request/VacationRequestDto.java
@@ -1,5 +1,6 @@
 package com.example.API.web.dto.request;
 
+import com.example.API.domain.user.User;
 import com.example.API.domain.vacation.Vacation;
 import com.example.API.domain.vacation.VacationStatus;
 import com.example.API.domain.vacation.VacationType;
@@ -13,27 +14,23 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class VacationRequestDto {
-    private VacationType vacationType;
-    private VacationStatus status;
-    private Double daysUsed;
+
     private LocalDate startDate;
     private LocalDate endDate;
-    private Double availableVacDays;
-    private Double requestedVacDays;
-    private Long userId;
+    private VacationType vacationType;
+    private VacationStatus status;
     private String comment;
 
-    public Vacation toEntity() {
+    public Vacation toEntity(User user, Double days_used, VacationStatus status,
+                             LocalDate startDate, LocalDate endDate) {
         return Vacation.builder()
+                .startDate(startDate)
+                .endDate(endDate)
                 .vacationType(this.vacationType)
-                .status(this.status)
-                .daysUsed(this.daysUsed)
-                .startDate(this.startDate)
-                .endDate(this.endDate)
                 .comment(this.comment)
-                .availableVacDays(this.availableVacDays)
-                .requestedVacDays(this.requestedVacDays)
-                .userId(this.userId)
+                .daysUsed(days_used)
+                .status(status)
+                .user(user)
                 .build();
     }
 }
